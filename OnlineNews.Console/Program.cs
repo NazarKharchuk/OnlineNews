ca3170368302c622e6960fdfeb09f5ac0aedd03b
Nazar <nazariykharchuk@gmail.com>
2022-12-11 17:35:56 +0200

    Добавьте файлы проекта.

---
+++ OnlineNews.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnlineNews.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ OnlineNews.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OnlineNews.DAL
+{
+    public class Class1
+    {
+    }
+}
