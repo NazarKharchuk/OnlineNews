a3a8e76465bae4e2e21e3b1ec2b2572e609e2548
Nazar <nazariykharchuk@gmail.com>
2022-12-13 01:32:36 +0200

    Some changes in BLL

---
+++ OnlineNews.BLL/Infrastructure/ServiceModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ninject.Modules;
+using OnlineNews.DAL.Interfaces;
+using OnlineNews.DAL.Repositories;
+
+namespace OnlineNews.BLL.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<UnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
--- OnlineNews.Cons/Program.cs
+++ OnlineNews.Cons/Program.cs
@@ -19,50 +19,12 @@ namespace OnlineNews.Cons
                     .UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=NewsDB;Trusted_Connection=True;")
                     .Options;
 
-            /*using (NewsContext DbContect = new NewsContext(options))
-            {
-                //Rubric r = new Rubric() { RubricName = "RubricName1" };
-                //DbContect.Rubrics.Add(r);
-                //DbContect.SaveChanges();
-
-                var rubrics = DbContect.Rubrics;
-                foreach(var i in rubrics)
-                {
-                    Console.WriteLine("id - {0}; name - {1}", i.RubricId, i.RubricName);
-                }
-            }*/
-
             UnitOfWork uow = new UnitOfWork(options);
 
-            /*Rubric r = uow.Rubrics.Find(t => t.RubricName == "RubricName1").First();
-            Console.WriteLine(r.RubricId);
-
-            News n = new News() { RubricId = r.RubricId, Title = "Title of news #1", Date = new DateTime(2017, 3, 25), Author = "The author of news #1", Content = "Content of news #1 written by author #1 in 2017." };
-            uow.News.Create(n);
-            Console.WriteLine(n.NewsId);
-
-            Tag t = uow.Tags.Find(t => t.TagName == "Tag name #2").First();
-            Console.WriteLine(t.TagId);
-
-            uow.News.AddTag(n, t);
-
-            uow.Save();
-
-            var news = uow.News.GetAll();
+             var news = uow.News.GetTags(1);
             foreach (var i in news)
             {
-                Console.WriteLine("id - {0}; title - {1}; date - {2}; author - {3}; content - {4}; rubric - {5}", i.NewsId, i.Title, i.Date, i.Author, i.Content, i.RubricId);
-
-            }*/
-            var news = uow.News.GetAll();
-            foreach (var i in news)
-            {
-                Console.WriteLine("id - {0}; title - {1};", i.NewsId, i.Title);
-                foreach (var t in i.Tags)
-                {
-                    Console.WriteLine("t id - {0}; t name - {1};", t.TagId, t.TagName);
-
-                }
+                Console.WriteLine("iD - {0}; Title - {1};", i.TagId, i.TagName);
             }
         }
     }
