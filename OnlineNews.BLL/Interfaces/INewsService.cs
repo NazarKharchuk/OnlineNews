942eb86b7f4b51b7627dcffa9668a0d4a9fbed2d
Nazar <nazariykharchuk@gmail.com>
2022-12-13 01:30:50 +0200

    Added interfaces in BLL

---
+++ OnlineNews.BLL/Interfaces/INewsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineNews.BLL.DTO;
+
+namespace OnlineNews.BLL.Interfaces
+{
+    public interface INewsService<NewsDTO> : IService<NewsDTO>
+    {
+        void AddTag(NewsDTO news, TagDTO tag);
+        IEnumerable<TagDTO> GetTags(int id);
+        IEnumerable<NewsDTO> FindByRubric(RubricDTO rubric);
+        IEnumerable<NewsDTO> FindByTag(TagDTO tag);
+        IEnumerable<NewsDTO> FindByAuthor(string author);
+        IEnumerable<NewsDTO> FindByDate(DateTime start, DateTime finish);
+    }
+}
---
+++ OnlineNews.BLL/Interfaces/ITagService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineNews.BLL.DTO;
+
+namespace OnlineNews.BLL.Interfaces
+{
+    public interface ITagService<TagDTO> : IService<TagDTO>
+    {
+        IEnumerable<NewsDTO> GetNews(int id);
+    }
+}
