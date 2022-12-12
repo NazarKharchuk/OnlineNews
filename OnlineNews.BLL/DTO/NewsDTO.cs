4e85d7e2d33c043d13480d205c31fcd8ca39462e
Nazar <nazariykharchuk@gmail.com>
2022-12-13 01:29:18 +0200

    Added data transfer objects

---
+++ OnlineNews.BLL/DTO/NewsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineNews.BLL.DTO
+{
+    public class NewsDTO
+    {
+        public int NewsId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public int RubricId { get; set; }
+    }
+}
---
+++ OnlineNews.BLL/DTO/RubricDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineNews.BLL.DTO
+{
+    public class RubricDTO
+    {
+        public int RubricId { get; set; }
+        public string RubricName { get; set; }
+    }
+}
