04fdf042b3af736738128f620233ac9353955461
Nazar <nazariykharchuk@gmail.com>
2022-12-12 02:48:51 +0200

    Added interfaces

---
+++ OnlineNews.DAL/Interfaces/INewsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineNews.DAL.Interfaces;
+using OnlineNews.DAL.Entities;
+
+namespace OnlineNews.DAL.Interfaces
+{
+    public interface INewsRepository<News> : IRepository<News>
+    {
+        void AddTag(News news, Tag tag);
+    }
+}
---
+++ OnlineNews.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineNews.DAL.Entities;
+
+namespace OnlineNews.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Rubric> Rubrics { get; }
+        INewsRepository<News> News { get; }
+        IRepository<Tag> Tags { get; }
+        void Save();
+    }
+}
