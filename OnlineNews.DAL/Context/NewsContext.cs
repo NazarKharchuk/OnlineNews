4e9d02ccaa54ae93169d5a6bdbcb26a208b85bae
Nazar <nazariykharchuk@gmail.com>
2022-12-12 02:48:14 +0200

    Added DBContext

---
+++ OnlineNews.DAL/Context/NewsContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineNews.DAL.Entities;
+
+namespace OnlineNews.DAL.Context
+{
+    public class NewsContext : DbContext
+    {
+        public DbSet<Rubric> Rubrics { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public NewsContext(DbContextOptions<NewsContext> options) 
+            :base(options) 
+        {
+            Database.EnsureCreated();
+        }
+        /*public NewsContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=NewsDB;Trusted_Connection=True;");
+        }*/
+    }
+}
