93fb4c0c3fc551f160a600e0a38b40e0130db4f6
Nazar <nazariykharchuk@gmail.com>
2022-12-15 22:24:52 +0200

    Added news model and controller

---
+++ OnlineNews.PL/Controllers/NewsController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineNews.BLL.DTO;
+using OnlineNews.BLL.Services;
+using OnlineNews.BLL.Interfaces;
+using OnlineNews.PL.Models;
+using AutoMapper;
+
+using Ninject.Modules;
+using Ninject;
+using OnlineNews.PL.Util;
+using OnlineNews.BLL.Infrastructure;
+using System.Net;
+
+namespace OnlineNews.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : Controller
+    {
+        INewsService<NewsDTO> newsService;
+        /*public NewsController(INewsService<NewsDTO> serv)
+        {
+            newsService = serv;
+        }*/
+
+        public NewsController()
+        {
+            NinjectModule newsModule = new NewsModule();
+
+            var kernel = new StandardKernel(newsModule);
+
+            newsService = kernel.Get<INewsService<NewsDTO>>();
+        }
+
+        // GET: api/<NewsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<NewsDTO, NewsModel>()).CreateMapper();
+            return new ObjectResult(mapper.Map<IEnumerable<NewsDTO>, List<NewsModel>>(newsService.GetAll()));
+        }
+
+        // GET api/<NewsController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var newsItem = newsService.Get(id);
+
+                return new ObjectResult(ItemFromDTO(newsItem));
+            }
+            catch (ValidationException ex)
+            {
+                return StatusCode(404, ex.Message);
+            }
+
+        }
+
+        // POST api/<NewsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] NewsModel item)
+        {
+            try
+            {
+                newsService.Create(DTOFromItem(item));
+
+                return StatusCode(201);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(400, ex.Message);
+            }
+        }
+
+        // PUT api/<NewsController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] NewsModel item)
+        {
+            if (id != item.RubricId)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                newsService.Update(DTOFromItem(item));
+
+                return NoContent();
+            }
+            catch (ValidationException ex)
+            {
+                return StatusCode(404, ex.Message);
+            }
+        }
+
+        // DELETE api/<NewsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var newsItem = newsService.Get(id);
+            }
+            catch (ValidationException ex)
+            {
+                return StatusCode(404, ex.Message);
+            }
+
+            newsService.Delete(id);
+            return NoContent();
+        }
+
+        private static NewsModel ItemFromDTO(NewsDTO item) =>
+            new NewsModel
+            {
+                NewsId = item.NewsId,
+                Title = item.Title,
+                Content = item.Content,
+                Author = item.Author,
+                Date = item.Date,
+                RubricId = item.RubricId
+            };
+
+        private static NewsDTO DTOFromItem(NewsModel item) =>
+            new NewsDTO
+            {
+                NewsId = item.NewsId,
+                Title = item.Title,
+                Content = item.Content,
+                Author = item.Author,
+                Date = item.Date,
+                RubricId = item.RubricId
+            };
+    }
+}
---
+++ OnlineNews.PL/Models/NewsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineNews.PL.Models
+{
+    public class NewsModel
+    {
+        public int NewsId { get; set; }
+        [Required(ErrorMessage = "Please enter title.")] 
+        [MaxLength(50, ErrorMessage = "The length of the title must be less than 50 characters.")] 
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter content.")] 
+        public string Content { get; set; }
+        [Required(ErrorMessage = "Please enter author.")] 
+        [MaxLength(30, ErrorMessage = "The length of the author name must be less than 30 characters.")] 
+        public string Author { get; set; }
+        [Required(ErrorMessage = "Please enter date.")] 
+        public DateTime Date { get; set; }
+        [Required(ErrorMessage = "Please enter rubric id.")] 
+        public int RubricId { get; set; }
+    }
+}
